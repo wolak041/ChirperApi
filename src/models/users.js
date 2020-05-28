33b7a078cc0cf3dd7a9493f344aa9309925cf797
kwolak <kwolak041@gmail.com>
2020-05-28 17:50:48 +0200

    server side validation

--- src/models/users.js
+++ src/models/users.js
@@ -10,17 +10,24 @@ const UserSchema = new mongose.Schema(
       trim: true,
       required: true,
       unique: true,
+      minlength: 3,
+      match: /^[a-zA-Z0-9_-]*$/,
     },
     email: {
       type: String,
       trim: true,
       required: true,
       unique: true,
+      validate: {
+        validator: (email) => /^([\w-.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(email),
+        message: 'Invalid email',
+      },
     },
     password: {
       type: String,
       required: true,
       select: false,
+      minlength: 6,
     },
   },
   {
