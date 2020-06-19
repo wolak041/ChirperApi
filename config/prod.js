ecb58d0f7a1a64e88a13d7be1a5a36a9b207dbe8
kwolak <kwolak041@gmail.com>
2020-06-19 20:34:05 +0200

    remove prod config template file

--- config/prod.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  MONGO: 'mongodb://localhost:27017/chirper',
-  PORT: 3000,
-  SESSION_SECRET: 'test',
-  MONGO_STORE_SECRET: 'test',
-  SESSION_UPDATE_AFTER: 24 * 60 * 60,
-  HOSTNAME: 'localhost',
-  SALT_ROUNDS: 10,
-};
