082c8ed8719ef64cd7275622ade2339a09d53e3e
kwolak <kwolak041@gmail.com>
2020-06-19 21:38:48 +0200

    config for heroku

--- config/common.js
+++ config/common.js
@@ -1,5 +1,5 @@
 const MODE = process.env.NODE_ENV;
-const config = require(`./${MODE === 'dev' ? 'dev' : 'prod'}`);
+const config = MODE === 'dev' ? require('./dev') : require('./prod');
 
 module.exports = {
   ...config,
