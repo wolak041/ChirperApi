8565e43004484324b539907b9b663d7d71422d8b
kwolak <kwolak041@gmail.com>
2020-06-19 21:19:40 +0200

    use env variables

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  MONGO: process.env.MONGO,
+  PORT: process.env.PORT,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+  MONGO_STORE_SECRET: process.env.MONGO_STORE_SECRET,
+  SESSION_UPDATE_AFTER: process.env.SESSION_UPDATE_AFTER,
+  HOSTNAME: process.env.HOSTNAME,
+  SALT_ROUNDS: process.env.SALT_ROUNDS,
+};
