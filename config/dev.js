30427aedfe11a52bdaf50b7d1b59f65539a19a00
kwolak <kwolak041@gmail.com>
2020-06-23 22:41:51 +0200

    Fix env variables

--- config/dev.js
+++ config/dev.js
@@ -1,10 +1,10 @@
 module.exports = {
   MONGO: 'mongodb://localhost:27017/chirper',
-  PORT: 3000,
+  MONGO_STORE_SECRET: 'test',
   JWT_SECRET: 'test',
   ACCESS_JWT_EXPIRATION_TIME: 20 * 60,
   REFRESH_JWT_EXPIRATION_TIME: 30 * 24 * 60 * 60,
-  MONGO_STORE_SECRET: 'test',
   HOSTNAME: '0.0.0.0',
+  PORT: 3000,
   ALLOW_ORIGIN: ['http://192.168.0.199', 'http://localhost'],
 };
